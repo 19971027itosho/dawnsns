30bdfb9014788383daf1610b458c35cc52df4246
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-17 14:45:37 +0900

    目的が登録したusernameを表示させるためにまず、authのuserに情報が入っているので＄authというショートカットキーを作成している

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Auth;
 
 class PostsController extends Controller
 {
     //
-    public function index(){
-        return view('posts.index');
+    public function index()
+    {
+        $posts = DB::table('posts')->get();
+        $auth = Auth::user();
+        return view('posts.index',['posts'=>$posts,'auth'=>$auth]);
     }
 }
