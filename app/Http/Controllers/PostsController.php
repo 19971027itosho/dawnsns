13cec643f2b9cfe36cf53790a5c455291eb00960
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-17 18:07:20 +0900

    一番上にpublic function __construct()     {         $this->middleware('auth');     } を移動。 理由は上から反映されていくから。

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,15 +9,15 @@ use Auth;
 class PostsController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $posts = DB::table('posts')->get();
         $auth = Auth::user();
         return view('posts.index',['posts'=>$posts,'auth'=>$auth]);
-    }
-
-    public function __construct()
-    {
-        $this->middleware('auth');
     }
 }
