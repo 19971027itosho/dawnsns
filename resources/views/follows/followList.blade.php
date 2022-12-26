1bf414e30f333b7943784b28395eb1dcbc627dd1
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-26 11:44:23 +0900

    画像抽出

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.login')
 
 @section('content')
+<h2>Follow List</h2>
+
+@foreach($users as $user)
+<img src="/images/{{ $user->images }}">
+@endforeach
+<br>
+@foreach($users as $user)
+    <tr>
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->username }}</td>
+        <td><a href="other-profile/{{ $user->id }}"><img src="/images/{{ $user->images }}"></a></td>
+        <td>{{ $user->posts }}</td>
+        <td>{{ $user->created_at }}</td>
+    </tr>
+    <br>
+@endforeach
 
 @endsection
