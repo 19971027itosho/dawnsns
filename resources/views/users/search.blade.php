8e7cf5387ea35db59505e597b923b348bb9cd624
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-26 11:47:34 +0900

    検索機能の機能充実

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.login')
 
 @section('content')
+{!! Form::open(['url' => 'result']) !!}
+            {!! Form::input('text', 'word', null, ['class' => 'form-control', 'placeholder' => 'ユーザー名']) !!}
+            <button type="submit" class="btn btn-success pull-right">検索</button>
+            {!! Form::close() !!}
 
-
-
+            <table class='table table-hover'>
+    @foreach ($users as $user)
+    <tr>
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->username }}</td>
+        <td><img src="/images/{{ $user->images }}"></td>
+        <td><a href="add-follow/{{$user->id}}">フォローする</a></td>
+        <td><a href="rem-follow/{{$user->id}}">フォロー外す</a></td>
+    </tr>
+    @endforeach
+</table>
 @endsection
