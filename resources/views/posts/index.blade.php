c4ae163b300a66adb33f5c503b8aba9207b70c89
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-26 11:46:19 +0900

    情報をどこから抽出するか記述

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,4 +3,34 @@
 @section('content')
 <h2>機能を実装していきましょう。</h2>
 
+{!! Form::open(['url' => 'post/create']) !!}
+            {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '何をつぶやこうか…？']) !!}
+            <button type="submit" class="btn btn-success pull-right"><img src="images/post.png" alt="post"></button>
+            {!! Form::close() !!}
+
+<table class='table table-hover'>
+    <tr>
+        <th>投稿No</th>
+        <th>投稿内容</th>
+        <th>投稿日時</th>
+    </tr>
+    @foreach ($posts as $post)
+    <tr>
+        <td>{{ $post->id }}</td>
+        <td>{{ $post->posts }}</td>
+        <td>{{ $post->created_at }}</td>
+        <td>
+        {!! Form::open(['url' => 'post/update']) !!}{!! Form::input('text', 'upPost', $post->posts , ['required', 'class' => 'form-control']) !!}
+        {{Form::hidden('id', $post->id)}}
+            <button type="submit" class="btn btn-success pull-right"><img src="images/edit.png" alt="post"></button>
+            {!! Form::close() !!}
+</td>
+        <td><a class="btn btn-danger" href="/post/{{ $post->id }}/delete" onclick="return confirm('こちらのつぶやきを削除します。よろしいでしょうか？')">
+                <img src="images/trash.png" alt="trash">
+            </a>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
 @endsection
