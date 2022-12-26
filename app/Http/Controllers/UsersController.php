8d6031a078b9f88be8aeb1ec8d66fed55bf80885
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-26 11:41:50 +0900

    searchについて記述

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use DB;
 
 class UsersController extends Controller
 {
@@ -10,7 +12,34 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
+
     public function search(){
-        return view('users.search');
+        $auth = Auth::user();
+        $follow_count =DB::table('follows')
+        ->where('follower',Auth::id())
+        ->count();
+        $follower_count =DB::table('follows')
+        ->where('follow',Auth::id())
+        ->count();
+        $users =DB::table('users')
+        ->where('id','!=',Auth::id())
+        ->get();
+        return view('users.search',compact('auth','follow_count','follower_count','users'));
+    }
+
+    public function result(Request $request){
+        $auth = Auth::user();
+        $follow_count =DB::table('follows')
+        ->where('follower',Auth::id())
+        ->count();
+        $follower_count =DB::table('follows')
+        ->where('follow',Auth::id())
+        ->count();
+        $key = $request->input('word');
+        $users =DB::table('users')
+        ->where('id','!=',Auth::id())
+        ->where('username','LIKE',"%".$key."%")
+        ->get();
+        return view('users.search',compact('auth','follow_count','follower_count','users'));//ほぼ同じ画面に出るので同じにしている
     }
 }
