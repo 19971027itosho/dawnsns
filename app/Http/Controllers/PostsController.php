eeee3cc36a4b1bea48b301bfd78b522e5fc76843
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-26 11:40:19 +0900

    取り出す値が、postindexの中の　$postsと$auth。

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,7 +17,57 @@ class PostsController extends Controller
     public function index()
     {
         $posts = DB::table('posts')->get();
+        // dd($posts);
         $auth = Auth::user();
-        return view('posts.index',['posts'=>$posts,'auth'=>$auth]);
+        $follow_count =DB::table('follows')
+        ->where('follower',Auth::id())
+        ->count();
+        $follower_count =DB::table('follows')
+        ->where('follow',Auth::id())
+        ->count();
+        return view('posts.index',['posts'=>$posts,'auth'=>$auth,'follow_count'=>$follow_count,'follower_count'=>$follower_count]);
     }
+
+    public function create(Request $request)
+    {
+        $id = Auth::id();
+        $post = $request->input('newPost');
+        DB::table('posts')->insert([
+            'user_id' => Auth::id(),
+            'posts' => $post
+        ]);
+        return redirect('top');
+    }
+
+    public function updateForm($id)
+    {
+        $post = DB::table('posts')
+            ->where('id', $id)
+            ->first();
+        return redirect('top');
+    }
+
+    public function update(Request $request)
+    {
+        $id = $request->input('id');
+        $up_post = $request->input('upPost');
+        DB::table('posts')
+            ->where('id', $id)
+            ->update(
+                ['posts' => $up_post]
+            );
+
+        return redirect('top');
+    }
+
+    public function delete($id)
+    {
+        // dd($id);
+        DB::table('posts')
+            ->where('id', $id)
+            ->delete();
+
+        return redirect('top');
+    }
+
 }
