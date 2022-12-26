b4bab644df53b90206430a916c1dc5ac739783a9
DESKTOP-3A78TDC\savan <savanna19971027@gmail.com>
2022-12-26 11:43:54 +0900

    ボタン作成

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.login')
 
 @section('content')
+<h2>Follower List</h2>
+<button type="submit" class="btn btn-success pull-right"><img src="images/dawn.png" alt="post"></button>
 @endsection
